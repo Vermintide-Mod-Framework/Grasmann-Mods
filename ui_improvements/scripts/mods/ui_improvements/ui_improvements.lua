940e7c47c04e9e93138c186bf706bb30c688fcab
grasmann <grasmann1805@gmail.com>
2018-05-25 17:24:14 +0200

    UIInprovements; Added switching career in hero view;

--- ui_improvements/scripts/mods/ui_improvements/ui_improvements.lua
+++ ui_improvements/scripts/mods/ui_improvements/ui_improvements.lua
@@ -24,25 +24,38 @@ local mod_data = {
 mod:initialize_data(mod_data)
 
 
-mod.window = nil
+mod.character_window = nil
+mod.career_window = nil
+
 mod.profile_index = nil
 mod.career_index = nil
 mod.orig_profile_by_peer = nil
+mod.orig_get_career = nil
 
 --[[
 	Functions
 --]]
-mod.create_window = function(self)
+mod.get_hero_view = function(self)
+	local matchmaking_manager = Managers.matchmaking
+	local ingame_ui = matchmaking_manager and matchmaking_manager._ingame_ui
+	local hero_view_active = ingame_ui and ingame_ui.current_view == "hero_view"
+	local hero_view = hero_view_active and ingame_ui.views["hero_view"]
+	return hero_view
+end
+
+mod.create_character_window = function(self)
 	
 	local simple_ui = get_mod("SimpleUI")
-	if simple_ui and not self.window then
+	if simple_ui and not self.character_window then
 		
 		local feedback = function(self)
 
-			local matchmaking_manager = Managers.matchmaking
-			local ingame_ui = matchmaking_manager and matchmaking_manager._ingame_ui
-			local hero_view_active = ingame_ui and ingame_ui.current_view == "hero_view"
-			local hero_view = hero_view_active and ingame_ui.views["hero_view"]
+			-- local matchmaking_manager = Managers.matchmaking
+			-- local ingame_ui = matchmaking_manager and matchmaking_manager._ingame_ui
+			-- local hero_view_active = ingame_ui and ingame_ui.current_view == "hero_view"
+			-- local hero_view = hero_view_active and ingame_ui.views["hero_view"]
+			
+			local hero_view = mod:get_hero_view()
 			
 			if hero_view then
 				mod:pcall(function()
@@ -70,7 +83,11 @@ mod.create_window = function(self)
 					-- Reopen view
 					--hero_view:init(ingame_ui_context)
 					hero_view:_change_screen_by_index(1)
-	
+					
+					mod.career_window:destroy()
+					mod.career_window = nil
+					mod:create_career_window(index)
+					
 				end)
 				
 			end
@@ -78,42 +95,265 @@ mod.create_window = function(self)
 		
 		local screen_width = RESOLUTION_LOOKUP.res_w
 		local screen_height = RESOLUTION_LOOKUP.res_h
-		local window_size = {545, 40}
+		local window_size = {705, 40}
 		
-		self.window = simple_ui:create_window("ui_improvements_hero_view", {screen_width / 3, screen_height - window_size[2]}, window_size)
+		self.character_window = simple_ui:create_window("ui_improvements_character", {120, screen_height - window_size[2] - 5}, window_size)
 		
-		local button = self.window:create_button("ui_improvements_hero_view_soldier", {5, 5}, {100, 30}, nil, "Soldier", 5)
+		local pos_x = 5
+		local button = self.character_window:create_button("ui_improvements_character_soldier", {pos_x, 5}, {130, 30}, nil, "Markus Kruber", 5)
 		button.on_click = feedback
+		pos_x = pos_x + 130 + 5
 		
-		local button = self.window:create_button("ui_improvements_hero_view_dwarf", {110, 5}, {100, 30}, nil, "Dwarf", 3)
+		local button = self.character_window:create_button("ui_improvements_character_dwarf", {pos_x, 5}, {155, 30}, nil, "Bardin Goreksson", 3)
 		button.on_click = feedback
+		pos_x = pos_x + 155 + 5
 		
-		local button = self.window:create_button("ui_improvements_hero_view_waywatcher", {220, 5}, {100, 30}, nil, "Waywatcher", 4)
+		local button = self.character_window:create_button("ui_improvements_character_waywatcher", {pos_x, 5}, {80, 30}, nil, "Kerillian", 4)
 		button.on_click = feedback
+		pos_x = pos_x + 80 + 5
 		
-		local button = self.window:create_button("ui_improvements_hero_view_witchhunter", {330, 5}, {100, 30}, nil, "Witchhunter", 1)
+		local button = self.character_window:create_button("ui_improvements_character_witchhunter", {pos_x, 5}, {145, 30}, nil, "Victor Saltzpyre", 1)
 		button.on_click = feedback
+		pos_x = pos_x + 145 + 5
 		
-		local button = self.window:create_button("ui_improvements_hero_view_wizard", {440, 5}, {100, 30}, nil, "Wizard", 2)
+		local button = self.character_window:create_button("ui_improvements_character_wizard", {pos_x, 5}, {165, 30}, nil, "Sienna Fuegonasus", 2)
 		button.on_click = feedback
+		pos_x = pos_x + 165 + 5
 		
-		self.window:init()
+		self.character_window:init()
 		
 	end
 	
 end
 
+mod.careers = {
+	{
+		{
+			name = "Witch Hunter Captain",
+			button_width = 190,
+			character_index = 1,
+			career_index = 1,
+		},
+		{
+			name = "Bounty Hunter",
+			button_width = 130,
+			character_index = 1,
+			career_index = 2,
+		},
+		{
+			name = "Zealot",
+			button_width = 70,
+			character_index = 1,
+			career_index = 3,
+		},
+	},
+	{
+		{
+			name = "Battle Wizard",
+			button_width = 130,
+			character_index = 2,
+			career_index = 1,
+		},
+		{
+			name = "Pyromancer",
+			button_width = 110,
+			character_index = 2,
+			career_index = 2,
+		},
+		{
+			name = "Unchained",
+			button_width = 100,
+			character_index = 2,
+			career_index = 3,
+		},
+	},
+	{
+		{
+			name = "Ranger Veteran",
+			button_width = 140,
+			character_index = 3,
+			career_index = 1,
+		},
+		{
+			name = "Ironbreaker",
+			button_width = 110,
+			character_index = 3,
+			career_index = 2,
+		},
+		{
+			name = "Slayer",
+			button_width = 70,
+			character_index = 3,
+			career_index = 3,
+		},
+	},
+	{
+		{
+			name = "Waystalker",
+			button_width = 110,
+			character_index = 4,
+			career_index = 1,
+		},
+		{
+			name = "Handmaiden",
+			button_width = 120,
+			character_index = 4,
+			career_index = 2,
+		},
+		{
+			name = "Shade",
+			button_width = 70,
+			character_index = 4,
+			career_index = 3,
+		},
+	},
+	{
+		{
+			name = "Mercenary",
+			button_width = 100,
+			character_index = 5,
+			career_index = 1,
+		},
+		{
+			name = "Huntsman",
+			button_width = 100,
+			character_index = 5,
+			career_index = 2,
+		},
+		{
+			name = "Foot Knight",
+			button_width = 110,
+			character_index = 5,
+			career_index = 3,
+		},
+	},
+}
 
+mod.create_career_window = function(self, profile_index)
 
-mod.reload_window = function(self)
-	self:destroy_window()
-	self:create_window()
+	local simple_ui = get_mod("SimpleUI")
+	if simple_ui and not self.career_window then
+		
+		local feedback = function(self)
+
+			-- local matchmaking_manager = Managers.matchmaking
+			-- local ingame_ui = matchmaking_manager and matchmaking_manager._ingame_ui
+			-- local hero_view_active = ingame_ui and ingame_ui.current_view == "hero_view"
+			-- local hero_view = hero_view_active and ingame_ui.views["hero_view"]
+			
+			local hero_view = mod:get_hero_view()
+			
+			if hero_view then
+				mod:pcall(function()
+
+					local ingame_ui_context = hero_view.ingame_ui_context
+					local index = self.params[1]
+					mod.profile_index = index
+					
+					-- Backup orig profile function
+					if not mod.orig_profile_by_peer then
+						mod.orig_profile_by_peer = ingame_ui_context.profile_synchronizer.profile_by_peer
+					end
+					
+					-- local profile_settings = SPProfiles[index]
+					-- local display_name = profile_settings.display_name
+					-- local hero_attributes = Managers.backend:get_interface("hero_attributes")
+					local career_index = self.params[2] --hero_attributes:get(display_name, "career")
+					mod.career_index = career_index
+					
+					-- Overwrite profile function
+					ingame_ui_context.profile_synchronizer.profile_by_peer = function(self, peer_id, local_player_id)
+						return index
+					end
+					
+					if not mod.orig_get_career then
+						mod.orig_get_career = Managers.backend._interfaces["hero_attributes"].get
+					end
+					
+					-- Overwrite career function
+					Managers.backend._interfaces["hero_attributes"].get = function(self, hero_name, attribute_name)
+						if attribute_name == "career" then
+							return career_index
+						end
+						return mod.orig_get_career(self, hero_name, attribute_name)
+					end
+					
+					-- mod:hook("BackendInterfaceHeroAttributes.get", function(func, self, hero_name, attribute_name)
+						-- if attribute_name == "career" then
+							-- return career_index
+						-- end
+						-- return func(self, hero_name, attribute_name)
+					-- end)
+					
+					-- Reopen view
+					--hero_view:init(ingame_ui_context)
+					hero_view:_change_screen_by_index(1)
+					
+					mod.career_window:destroy()
+					mod.career_window = nil
+					mod:create_career_window(index)
+					
+				end)
+				
+			end
+		end
+		
+		local screen_width = RESOLUTION_LOOKUP.res_w
+		local screen_height = RESOLUTION_LOOKUP.res_h
+		local window_size = {500, 40}
+		
+		self.career_window = simple_ui:create_window("ui_improvements_career", {120, screen_height - window_size[2] - 45}, window_size)
+		
+		local careers = self.careers[profile_index]
+		
+		if careers then
+			
+			local pos_x = 5
+			local window_width = 5
+			
+			for _, career in pairs(careers) do
+				
+				--self:echo(career.name)
+				local button = self.career_window:create_button("ui_improvements_career_"..career.name, {pos_x, 5}, {career.button_width, 30}, nil, career.name, {career.character_index, career.career_index})
+				button.on_click = feedback
+				pos_x = pos_x + career.button_width + 5
+				window_width = window_width + career.button_width + 5
+				
+			end
+			
+			self.career_window.size[1] = window_width
+			
+		else
+		
+			self:echo("nope")
+			self:echo("index: "..tostring(profile_index))
+			
+		end
+		
+		self.career_window:init()
+		
+	end
+	
 end
 
-mod.destroy_window = function(self)
-	if self.window then
-		self.window:destroy()
-		self.window = nil
+mod.reload_windows = function(self)
+	self:destroy_windows()
+	-- Character
+	self:create_character_window()
+	-- Career
+	local player = Managers.player:local_player()
+	local profile_index = player:profile_index()
+	self:create_career_window(profile_index)
+end
+
+mod.destroy_windows = function(self)
+	if self.character_window then
+		self.character_window:destroy()
+		self.character_window = nil
+	end
+	if self.career_window then
+		self.career_window:destroy()
+		self.career_window = nil
 	end
 end
 
@@ -177,7 +417,7 @@ mod:hook("HeroView.on_enter", function(func, self, menu_state_name, menu_sub_sta
 	-- Orig function
 	func(self, menu_state_name, menu_sub_state_name)
 	-- Reload window
-	mod:reload_window()
+	mod:reload_windows()
 end)
 --[[
 	Create window when unsuspending hero view
@@ -186,7 +426,7 @@ mod:hook("HeroView.unsuspend", function(func, self)
 	-- Orig function
 	func(self)
 	-- Reload window
-	mod:reload_window()
+	mod:reload_windows()
 end)
 --[[
 	Destroy window when closing hero view
@@ -197,12 +437,17 @@ mod:hook("HeroView.on_exit", function(func, self)
 		self.ingame_ui_context.profile_synchronizer.profile_by_peer = mod.orig_profile_by_peer
 		mod.orig_profile_by_peer = nil
 	end
+	-- Reset career function
+	if not mod.orig_get_career then
+		Managers.backend._interfaces["hero_attributes"].get = mod.orig_get_career
+		mod.orig_get_career = nil
+	end
 	mod.profile_index = nil
 	mod.career_index = nil
 	-- Orig function
 	func(self)
 	-- Destroy window
-	mod:destroy_window()
+	mod:destroy_windows()
 end)
 
 --[[
