103c5f40e26ab132837b14c8f3d92cf0f1241788
bi <gkadet@gmail.com>
2018-06-12 14:26:23 +0300

    More Rat Weapons: updated for latest VMF

--- more_rat_weapons/scripts/mods/more_rat_weapons/more_rat_weapons_data.lua
+++ more_rat_weapons/scripts/mods/more_rat_weapons/more_rat_weapons_data.lua
@@ -11,8 +11,7 @@ mod_data.options_widgets = {
 		["widget_type"] = "numeric",
 		["text"] = "Slave Rat Shield Chance",
 		["unit_text"] = " %",
-		["tooltip"] =  "Slave Rate Shield Chance\n" ..
-			"Set the chance for slave rats to have shields.",
+		["tooltip"] =  "Set the chance for slave rats to have shields.",
 		["range"] = {0, 100},
 		["decimals_number"] = 0,
 		["default_value"] = 25,
@@ -22,8 +21,7 @@ mod_data.options_widgets = {
 		["widget_type"] = "numeric",
 		["text"] = "Clan Rat Shield Chance",
 		["unit_text"] = " %",
-		["tooltip"] =  "Clan Rat Shield Chance\n" ..
-			"Set the chance for clan rats to have shields.",
+		["tooltip"] =  "Set the chance for clan rats to have shields.",
 		["range"] = {0, 100},
 		["decimals_number"] = 0,
 		["default_value"] = 25,
@@ -33,8 +31,7 @@ mod_data.options_widgets = {
 		["widget_type"] = "numeric",
 		["text"] = "Stormvermin Shield Chance",
 		["unit_text"] = " %",
-		["tooltip"] =  "Stormvermin Shield Chance\n" ..
-			"Set the chance for stormvermin to have shields.",
+		["tooltip"] =  "Set the chance for stormvermin to have shields.",
 		["range"] = {0, 100},
 		["decimals_number"] = 0,
 		["default_value"] = 25,
@@ -43,24 +40,21 @@ mod_data.options_widgets = {
 		["setting_name"] = "remove_running_attacks",
 		["widget_type"] = "checkbox",
 		["text"] = "Remove Running Attacks",
-		["tooltip"] =  "Remove Running Attacks\n" ..
-			"Removes the running attacks from rats that still have their shield.",
+		["tooltip"] =  "Removes the running attacks from rats that still have their shield.",
 		["default_value"] = true,
 	},
 	{
 		["setting_name"] = "shield_mechanics",
 		["widget_type"] = "checkbox",
 		["text"] = "Shield Mechanics",
-		["tooltip"] =  "Shields Mechanics\n" ..
-			"Rats can block attacks with their shields.",
+		["tooltip"] =  "Rats can block attacks with their shields.",
 		["default_value"] = false,
 		["sub_widgets"] = {
 			{
 				["setting_name"] = "shield_type",
 				["widget_type"] = "dropdown",
 				["text"] = "Shields Type",
-				["tooltip"] =  "Shields Type\n" ..
-					"Choose how the shields are implemented.\n\n" ..
+				["tooltip"] = "Choose how the shields are implemented.\n\n" ..
 					"--- Health ---\n" ..
 					"Shields have a certain health depending on the difficulty\n\n" ..
 					"--- Points ---\n" ..
@@ -78,8 +72,7 @@ mod_data.options_widgets = {
 				["setting_name"] = "play_comments",
 				["widget_type"] = "checkbox",
 				["text"] = "Play Armour Comments",
-				["tooltip"] =  "Play Armour Comments\n" ..
-					"Triggers comments about hitting armour on shield hits.",
+				["tooltip"] = "Triggers comments about hitting armour on shield hits.",
 				["default_value"] = true,
 				["sub_widgets"] = {
 					{
@@ -87,8 +80,7 @@ mod_data.options_widgets = {
 						["widget_type"] = "numeric",
 						["text"] = "Pause Between Comments",
 						["unit_text"] = " sec",
-						["tooltip"] =  "Pause Between Comments\n" ..
-							"Seconds between armour comments.",
+						["tooltip"] =  "Seconds between armour comments.",
 						["range"] = {10, 120},
 						["decimals_number"] = 0,
 						["default_value"] = 30,
@@ -98,8 +90,7 @@ mod_data.options_widgets = {
 						["widget_type"] = "numeric",
 						["text"] = "Comment Distance",
 						["unit_text"] = " units",
-						["tooltip"] =  "Comment Distance\n" ..
-							"The maximum distance another hero can be away to comment.",
+						["tooltip"] = "The maximum distance another hero can be away to comment.",
 						["range"] = {3, 30},
 						["decimals_number"] = 0,
 						["default_value"] = 10,
@@ -110,24 +101,21 @@ mod_data.options_widgets = {
 				["setting_name"] = "play_particle_effects",
 				["widget_type"] = "checkbox",
 				["text"] = "Play Particle Effects",
-				["tooltip"] =  "Play Particle Effects\n" ..
-					"Triggers particle effects on shield hits.",
+				["tooltip"] = "Triggers particle effects on shield hits.",
 				["default_value"] = true,
 			},
 			{
 				["setting_name"] = "play_shield_sounds",
 				["widget_type"] = "checkbox",
 				["text"] = "Play Shield Sounds",
-				["tooltip"] =  "Play Shield Sounds\n" ..
-					"Triggers sound effects on shield hits.",
+				["tooltip"] = "Triggers sound effects on shield hits.",
 				["default_value"] = true,
 			},
 			{
 				["setting_name"] = "drop_shields",
 				["widget_type"] = "checkbox",
 				["text"] = "Physically Drop Shields",
-				["tooltip"] =  "Physically Drop Shields\n" ..
-					"Physically drop shields after the health is empty.",
+				["tooltip"] = "Physically drop shields after the health is empty.",
 				["default_value"] = true,
 			},
 		},
@@ -137,8 +125,7 @@ mod_data.options_widgets = {
 		-- --["widget_type"] = "stepper",
 		-- ["widget_type"] = "checkbox",
 		-- ["text"] = "Rats Use Player Weapons",
-		-- ["tooltip"] =  "Rats Use Player Weapons [Local]\n" ..
-			-- "Randomly give rats player weapons.",
+		-- ["tooltip"] =  "Randomly give rats player weapons.",
 		-- -- ["options"] = {
 			-- -- {text = "Off", value = false},
 			-- -- {text = "On", value = true},
@@ -149,8 +136,7 @@ mod_data.options_widgets = {
 				-- ["setting_name"] = "player_weapons_count",
 				-- ["widget_type"] = "stepper",
 				-- ["text"] = "Weapon Count",
-				-- ["tooltip"] =  "Weapon Count [Local]\n" ..
-					-- "How many player weapons should be loaded and used?\n\n" ..
+				-- ["tooltip"] = "How many player weapons should be loaded and used?\n\n" ..
 					-- "--- Default ---\n" ..
 					-- "17 Weapons and 4 Shields.\n" ..
 					-- "--- More 1 ---\n" ..
