99d70f7667888688a28dfd6e772a7a6da403e4c9
grasmann <grasmann1805@gmail.com>
2018-06-12 16:31:35 +0200

    LoadoutManager; Update for latest vmf;

--- loadout_manager/scripts/mods/loadout_manager/loadout_manager.lua
+++ loadout_manager/scripts/mods/loadout_manager/loadout_manager.lua
@@ -4,7 +4,7 @@ local mod = get_mod("LoadoutManager")
 		- Saves load out settings
 
 	Author: walterr, iamlupo & grasmann
-	Version: 2.0.0
+	Version: 2.0.2
 --]]
 
 -- ##### ██████╗  █████╗ ████████╗ █████╗ #############################################################################
