ee6d098e7e779091381e3a46e7c054755823147e
Azumgi <4zumgi@gmail.com>
2019-01-17 13:57:56 +0300

    ChatBlock; Added Russian localization;

--- chat_block/scripts/mods/chat_block/chat_block_localization.lua
+++ chat_block/scripts/mods/chat_block/chat_block_localization.lua
@@ -1,9 +1,11 @@
 return {
 	mod_description = {
 		en = "Block attacks when typing in chat or using ui menus.",
+		ru = "Автоматически ставит блок, когда игрок открывает чат или другие элементы интерфейса.",
 	},
 	mode = {
 		en = "Mode",
+		ru = "Режим блока",
 	},
 	mode_description = {
 		en = "Automatically block when you're chatting.\n\n"..
@@ -12,11 +14,17 @@ return {
 		"Blocking animation will play, works only with melee weapons.\n\n"..
 		"-- ANIMATION AND PUSH --\n"..
 		"You will also push when closing the chat window.",
+		ru = "-- БЛОК --\n"..
+		"Блокирует удары.\n\n"..
+		"-- БЛОК И ТОЛЧОК --\n"..
+		"Блокирует удары и делает толчок, когда игрок закрывает меню.",
 	},
 	animation = {
 		en = "Animation",
+		ru = "Блок",
 	},
 	animation_and_push = {
 		en = "Animation and Push",
+		ru = "Блок и толчок",
 	},
 }
