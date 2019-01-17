ef808999fdc3c112044b6ba4b0f4b651e8be421c
Azumgi <4zumgi@gmail.com>
2019-01-17 13:31:22 +0300

    ChatBlock; Updated widgets definition;

--- chat_block/scripts/mods/chat_block/chat_block_localization.lua
+++ chat_block/scripts/mods/chat_block/chat_block_localization.lua
@@ -1,5 +1,22 @@
 return {
 	mod_description = {
-		en = "Block attacks when typing in chat or using ui menus."
-	}
+		en = "Block attacks when typing in chat or using ui menus.",
+	},
+	mode = {
+		en = "Mode",
+	},
+	mode_description = {
+		en = "Automatically block when you're chatting.\n\n"..
+		"Your block will still break if your stamina runs out.\n\n"..
+		"-- ANIMATION --\n"..
+		"Blocking animation will play, works only with melee weapons.\n\n"..
+		"-- ANIMATION AND PUSH --\n"..
+		"You will also push when closing the chat window.",
+	},
+	animation = {
+		en = "Animation",
+	},
+	animation_and_push = {
+		en = "Animation and Push",
+	},
 }
