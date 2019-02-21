2cc26f80a2e0bf531edfc44aeabb49141f3ff3e0
grasmann <grasmann1805@gmail.com>
2019-02-21 03:18:46 +0100

    ChatBlock; Localization in english, russian, german;

--- chat_block/scripts/mods/chat_block/chat_block_localization.lua
+++ chat_block/scripts/mods/chat_block/chat_block_localization.lua
@@ -2,29 +2,35 @@ return {
 	mod_description = {
 		en = "Block attacks when typing in chat or using ui menus.",
 		ru = "Автоматически ставит блок, когда игрок открывает чат или другие элементы интерфейса.",
+		de = "Blocke Angriffe beim Chatten und in Spielmenüs.",
 	},
 	mode = {
 		en = "Mode",
 		ru = "Режим блока",
+		de = "Modus",
 	},
 	mode_description = {
-		en = "Automatically block when you're chatting.\n\n"..
-		"Your block will still break if your stamina runs out.\n\n"..
-		"-- ANIMATION --\n"..
-		"Blocking animation will play, works only with melee weapons.\n\n"..
+		en = "-- ANIMATION --\n"..
+		"Blocks attacks.\n\n"..
 		"-- ANIMATION AND PUSH --\n"..
-		"You will also push when closing the chat window.",
+		"Blocks attacks and does a push on exit.",
 		ru = "-- БЛОК --\n"..
 		"Блокирует удары.\n\n"..
 		"-- БЛОК И ТОЛЧОК --\n"..
 		"Блокирует удары и делает толчок, когда игрок закрывает меню.",
+		de = "-- ANIMATION --\n"..
+		"Blockiert Angriffe\n\n"..
+		"-- ANIMATION UND STOß --\n"..
+		"Blockiert Angriffe und führt am Ende einen Stoß aus.",
 	},
 	animation = {
 		en = "Animation",
 		ru = "Блок",
+		de = "Animation",
 	},
 	animation_and_push = {
 		en = "Animation and Push",
 		ru = "Блок и толчок",
+		de = "Animation und Stoß",
 	},
 }
