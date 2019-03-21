7a2cc567120dba1e82a27a83c13fec847f76cbd8
grasmann <grasmann1805@gmail.com>
2019-03-21 09:34:33 +0100

    AllReady; Localized in english and german; Workshop stuff;

--- all_ready/scripts/mods/all_ready/all_ready_localization.lua
+++ all_ready/scripts/mods/all_ready/all_ready_localization.lua
@@ -1,5 +1,6 @@
 return {
 	mod_description = {
-		en = "Lets you start the game without waiting for people to press ready."
+		en = "Lets you start the game without waiting for people to press ready.",
+		de = "Das Spiel kann gestartet werden ohne dass die Spieler bereit drücken.",
 	}
 }
